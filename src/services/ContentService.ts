535f8462910530b2663d729a55678b218d115e35
Deeksha Deepak <deekshad1502@gmail.com>
2026-02-02 11:35:02 +0530

    removed content service files

--- src/services/ContentService.ts
+++
@@ -1,7 +0,0 @@
-import { getClient, ApiResponse } from '../lib/http-client';
-
-export class ContentService {
-  public async getContent<T = any>(): Promise<ApiResponse<T>> {
-    return getClient().get<T>('/content');
-  }
-}
