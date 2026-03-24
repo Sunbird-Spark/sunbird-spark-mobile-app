f2342cf9adc5eb7f1db8837bdd2cc00c8c4eb1ef
Rakshitha-D <rakshithagowda.d@gmail.com>
2026-03-24 12:12:38 +0530

    feat(assets): add notification and trash icons as SVG assets

--- src/components/notifications/NotificationCard.tsx
+++ src/components/notifications/NotificationCard.tsx
@@ -7,6 +7,7 @@ import 'dayjs/locale/fr';
 import 'dayjs/locale/pt';
 import type { NotificationFeed } from '../../types/notificationTypes';
 import { useNotificationMessage } from '../../hooks/useNotifications';
+import trashIcon from '../../assets/trash.svg';
 
 interface NotificationCardProps {
   notification: NotificationFeed;
@@ -17,13 +18,6 @@ interface NotificationCardProps {
 function formatTimestamp(dateStr: string, locale: string): string {
   return dayjs(dateStr).locale(locale).format('ddd, DD MMMM, hh:mm a');
 }
-
-const TrashIcon = () => (
-  <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#CC8545" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-    <polyline points="3 6 5 6 21 6" />
-    <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
-  </svg>
-);
 
 const NotificationCard: React.FC<NotificationCardProps> = ({
   notification,
@@ -62,7 +56,7 @@ const NotificationCard: React.FC<NotificationCardProps> = ({
         }}
         aria-label={t('deleteNotification')}
       >
-        <TrashIcon />
+        <img src={trashIcon} alt="" width="18" height="19" />
       </button>
     </div>
   );
