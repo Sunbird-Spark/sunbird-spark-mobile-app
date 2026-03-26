d6c3a63dfdc8044225aa5525943c3d6150e94d15
Rakshitha-D <rakshithagowda.d@gmail.com>
2026-03-26 15:47:32 +0530

    fix(ContentPlayerPage): conditionally render thumbnail image when available

--- src/pages/ContentPlayerPage.tsx
+++ src/pages/ContentPlayerPage.tsx
@@ -383,11 +383,13 @@ const ContentPlayerPage: React.FC = () => {
                 onClick={handlePlay}
                 aria-label={`Play ${playerMetadata.name}`}
               >
-                <IonImg
+                {(playerMetadata.posterImage || playerMetadata.appIcon) && (
+                  <IonImg
                     src={playerMetadata.posterImage || playerMetadata.appIcon}
                     alt={playerMetadata.name}
                     className="cp-thumbnail"
-                />
+                  />
+                )}
                 <div className="cp-play-button">
                   <svg width="12" height="14" viewBox="0 0 12 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M12 7L0.75 13.4952L0.75 0.504809L12 7Z" fill="var(--ion-color-primary)" />
